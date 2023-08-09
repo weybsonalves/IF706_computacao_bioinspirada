8d2992846e9421aada06785de19055af89eeeeee
weybsonalves <was5@cin.ufpe.br>
2023-08-08 21:30:17 -0300

    Evolution Strategy

--- Atividade 2/EA/EvolutionStrategy.py
+++ Atividade 2/EA/EvolutionStrategy.py
@@ -2,15 +2,15 @@ import numpy as np
 import random
 import math
 
-class GeneticAlgorithm():
-    def __init__(self, tamanho_individuo=30, tamanho_populacao=500, n_filhos=2, tamanho_torneio=5, max_iteracoes=10000, prob_cruzamento=0.9, prob_mutacao=0.4, funcao='ackley'):
+class EvolutionStrategy():
+    def __init__(self, tamanho_individuo=30, tamanho_populacao=500, n_filhos=3500, tamanho_torneio=10, max_iteracoes=10000, prob_cruzamento=0.3, learning_rate=0.1, funcao='ackley'):
         self.tamanho_individuo = tamanho_individuo
         self.tamanho_populacao = tamanho_populacao
         self.n_filhos = n_filhos
         self.tamanho_torneio = tamanho_torneio
         self.max_iteracoes = max_iteracoes
         self.prob_cruzamento = prob_cruzamento
-        self.prob_mutacao = prob_mutacao
+        self.learning_rate = learning_rate
         self.funcao = funcao
 
         self.populacao = []
@@ -19,30 +19,30 @@ class GeneticAlgorithm():
 
     def gerar_individuo(self):
         if self.funcao == 'ackley':
-            individuo = np.array([random.uniform(-32.768, 32.768) for i in range(self.tamanho_individuo)])
+            individuo = (np.array([random.uniform(-32.768, 32.768) for i in range(self.tamanho_individuo)]), 5)
         elif self.funcao == 'rastrigin':
-            individuo = np.array([random.uniform(-5.12, 5.12) for i in range(self.tamanho_individuo)])
+            individuo = (np.array([random.uniform(-5.12, 5.12) for i in range(self.tamanho_individuo)]), 5)
         elif self.funcao == 'schwefel':
-            individuo = np.array([random.uniform(-500, 500) for i in range(self.tamanho_individuo)])
+            individuo = (np.array([random.uniform(-500, 500) for i in range(self.tamanho_individuo)]), 5)
         elif self.funcao == 'rosenbrock':
-            individuo = np.array([random.uniform(-5, 10) for i in range(self.tamanho_individuo)])
+            individuo = (np.array([random.uniform(-5, 10) for i in range(self.tamanho_individuo)]), 5)
         else:
             raise ValueError('Função inválida.')
-        return individuo.copy()
+        return individuo
 
     def calcular_fitness(self, individuo):
         if self.funcao == 'ackley':
-            termo_1 = -20 * np.exp(-0.2*np.sqrt(np.sum(individuo**2)) / self.tamanho_individuo)
-            termo_2 = np.exp(np.sum(np.cos(individuo*2*np.pi)) / self.tamanho_individuo)
+            termo_1 = -20 * np.exp(-0.2*np.sqrt(np.sum(individuo[0]**2)) / self.tamanho_individuo)
+            termo_2 = np.exp(np.sum(np.cos(individuo[0]*2*np.pi)) / self.tamanho_individuo)
             fitness = termo_1 - termo_2 + 20 + np.exp(1)
         elif self.funcao == 'rastrigin':
-            fitness = 10*self.tamanho_individuo + np.sum(individuo**2 - 10*np.cos(individuo*2*np.pi))
+            fitness = 10*self.tamanho_individuo + np.sum(individuo[0]**2 - 10*np.cos(individuo[0]*2*np.pi))
         elif self.funcao == 'schwefel':
-            fitness = 418.9829*self.tamanho_individuo - np.sum(individuo * np.sin(np.sqrt(np.abs(individuo))))
+            fitness = 418.9829*self.tamanho_individuo - np.sum(individuo[0] * np.sin(np.sqrt(np.abs(individuo[0]))))
         elif self.funcao == 'rosenbrock':
             fitness = 0
             for i in range(self.tamanho_individuo - 1):
-                fitness += 100*(individuo[i + 1] - individuo[i]**2)**2 + (individuo[i] - 1)**2
+                fitness += 100*(individuo[0][i + 1] - individuo[0][i]**2)**2 + (individuo[0][i] - 1)**2
         return fitness
 
     def selecionar_sobreviventes(self, filhos):
@@ -51,28 +51,33 @@ class GeneticAlgorithm():
         return populacao_ordenada[:self.tamanho_populacao]
 
     def mutacao(self, individuo):
+        individuo_mutado = individuo[0].copy()
+        parametro_mutado = individuo[1]
         for i in range(self.tamanho_individuo):
-            if random.random() < self.prob_mutacao:
-                if self.funcao == 'ackley':
-                    individuo[i] = max(min(individuo[i] * np.random.normal(0, 1), 32.768), -32.768)
-                elif self.funcao == 'rastrigin':
-                    individuo[i] = max(min(individuo[i] * np.random.normal(0, 1), 5.12), -5.12)
-                elif self.funcao == 'schwefel':
-                    individuo[i] = max(min(individuo[i] * np.random.normal(0, 5), 500), -500)
-                elif self.funcao == 'rosenbrock':
-                    individuo[i] = max(min(individuo[i] * np.random.normal(0, 1), 10), -5)
-        return individuo
+            if self.funcao == 'ackley':
+                parametro_mutado = parametro_mutado * np.exp(self.learning_rate * np.random.normal(0, 1))
+                individuo_mutado[i] = max(min(individuo_mutado[i] * np.random.normal(0, parametro_mutado), 32.768), -32.768)
+            elif self.funcao == 'rastrigin':
+                parametro_mutado = parametro_mutado * np.exp(self.learning_rate * np.random.normal(0, 1))
+                individuo_mutado[i] = max(min(individuo_mutado[i] * np.random.normal(0, parametro_mutado), 5.12), -5.12)
+            elif self.funcao == 'schwefel':
+                parametro_mutado = parametro_mutado * np.exp(self.learning_rate * np.random.normal(0, 1))
+                individuo_mutado[i] = max(min(individuo_mutado[i] * np.random.normal(0, parametro_mutado), 500), -500)
+            elif self.funcao == 'rosenbrock':
+                parametro_mutado = parametro_mutado * np.exp(self.learning_rate * np.random.normal(0, 1))
+                individuo_mutado[i] = max(min(individuo_mutado[i] * np.random.normal(0, parametro_mutado), 10), -5)
+        return (individuo_mutado, parametro_mutado)
 
     def cruzamento(self, pai_1, pai_2):
         if random.random() < self.prob_cruzamento:
             ponto_corte = random.randint(1, self.tamanho_individuo - 1)
             
-            filho_1 = np.concatenate((pai_1[:ponto_corte], pai_2[ponto_corte:]))
-            filho_2 = np.concatenate((pai_2[:ponto_corte], pai_1[ponto_corte:]))
+            filho_1 = np.concatenate((pai_1[0][:ponto_corte], pai_2[0][ponto_corte:]))
+            filho_2 = np.concatenate((pai_2[0][:ponto_corte], pai_1[0][ponto_corte:]))
 
-            return filho_1.copy(), filho_2.copy()
+            return (filho_1.copy(), pai_1[1]), (filho_2.copy(), pai_2[1])
         else:
-            return pai_1.copy(), pai_2.copy()
+            return pai_1, pai_2
 
     def selecionar_pais(self):
         candidatos = random.sample(self.populacao, self.tamanho_torneio)
@@ -90,7 +95,7 @@ class GeneticAlgorithm():
         melhor_fitness = self.calcular_fitness(melhor_individuo)
 
         geracao = dict()
-        geracao['populacao'] = [individuo.copy() for individuo in self.populacao]
+        geracao['populacao'] = [individuo for individuo in self.populacao]
         geracao['fitness'] = [self.calcular_fitness(individuo) for individuo in self.populacao]
         geracao['melhor_individuo'] = melhor_individuo
         geracao['melhor_fitness'] = melhor_fitness
@@ -114,7 +119,7 @@ class GeneticAlgorithm():
             melhor_individuo = min(self.populacao, key=self.calcular_fitness)
             melhor_fitness = self.calcular_fitness(melhor_individuo)
             geracao = dict()
-            geracao['populacao'] = [individuo.copy() for individuo in self.populacao]
+            geracao['populacao'] = [individuo for individuo in self.populacao]
             geracao['fitness'] = [self.calcular_fitness(individuo) for individuo in self.populacao]
             geracao['melhor_individuo'] = melhor_individuo
             geracao['melhor_fitness'] = melhor_fitness
